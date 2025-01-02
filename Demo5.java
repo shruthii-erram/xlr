ce1f86429d8c6039d870b3f5995ef0a27a368951
shruthii-erram <shruthi27092005@gmail.com>
2025-01-02 22:51:00 +0530

    Add files via upload
    demo 4 java program

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+class Demo5{
+    public static void main(String[] args){
+        String username="user_001";
+        String pwd="pwd_001";
+        while((username=="user_001")&&(pwd=="pwd_001")){
+            System.out.println("hello world");
+            break;
+        }
+    }
+}
