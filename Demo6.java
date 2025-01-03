9e0be76cfb1df1292e9543972bfe1b1987050894
shruthii-erram <shruthi27092005@gmail.com>
2025-01-03 10:38:32 +0530

    Add files via upload
    username and password

---
+++ Demo6.java
@@ -0,0 +1,10 @@
+class Demo6{
+    public static void main(String[] args){
+        String username="shruthi_001";
+        String pwd="shruthii_001";
+        while((username=="shruthi_001")&&(pwd=="shruthii_001")){
+            System.out.println("hello world");
+            break;
+        }
+    }
+}
