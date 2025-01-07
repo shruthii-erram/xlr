70183c23d6d75a2aefc6d532a92173344b600b1b
shruthii-erram <shruthi27092005@gmail.com>
2025-01-07 11:53:34 +0530

    Add files via upload
    Unary operators

---
+++ Demo14.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class UnaryDemo {
+    void UnaryOp() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value for a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter value for b: ");
+        int b = scanner.nextInt();
+        System.out.println("a++: " + a++);
+        System.out.println("++a: " + ++a);
+        System.out.println("b--: " + b--);
+        System.out.println("--b: " + --b);
+        a = 2; 
+        b = 5;
+        int c = a++ * --b * a;
+        System.out.println("Result of a++ * --b * a: " + c);
+        
+        scanner.close(); 
+    }
+}
+
+public class Demo14 {
+    public static void main(String[] args) {
+        UnaryDemo obj = new UnaryDemo();
+        obj.UnaryOp();
+    }
+}
