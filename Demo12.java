0820f9a2faff598c5300610151a3c47ec8162a1d
shruthii-erram <shruthi27092005@gmail.com>
2025-01-07 11:39:47 +0530

    Add files via upload
    area of circle

---
+++ Demo12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+   class CircleAreaDemo1 {
+
+	void Area() {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+
+		
+		int r = scanner.nextInt();
+		
+		double ca = Math.PI * (r*r);
+		
+		System.out.println("The circle area is : " + ca);
+	}
+
+}
+ public class Demo12 {
+    public static void main(String[] args) {
+        CircleAreaDemo1 obj = new CircleAreaDemo1();
+        obj.Area();}
+
+ }
