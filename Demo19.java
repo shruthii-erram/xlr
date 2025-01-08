06cc97603a385a34178517b88d43ffb8ad9a29ea
shruthii-erram <shruthi27092005@gmail.com>
2025-01-08 11:37:53 +0530

    Add files via upload
    The next multiple of 5 after given number

---
+++ Demo19.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Demo19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int roundFig = ((number / 5) + 1) * 5;
+        System.out.println("The next multiple of 5 after  " + number + " is: " +roundFig);
+
+        scanner.close();
+    }
+}
