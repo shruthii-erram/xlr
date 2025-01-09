ae0bb6412d21e4e3b20c7a4d93f7cc197086d49d
shruthii-erram <shruthi27092005@gmail.com>
2025-01-09 12:38:12 +0530

    Add files via upload
    Multiple of 3,5,3 and 5(Fizz,FizzBzz)

--- Demo20.java
+++ Demo20.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
-
+class Fizz {
+    public static String getFizzBizz(int value) {
+        if (value <= 0) {
+            return "Error";
+        } else if (value % 3 == 0 && value % 5 == 0) {
+            return "FIZZBIZZ";
+        } else if (value % 3 == 0) {
+            return "FIZZ";
+        } else if (value % 5 == 0) {
+            return "BIZZ";
+        } else {
+            return String.valueOf(value);
+        }
+    }
+}
 public class Demo20 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of terms for Fibonacci sequence: ");
-        int n = scanner.nextInt();
-        System.out.println("Fibonacci sequence up to " + n + " terms:");
-        long first = 0, second = 1;
-        if (n >= 1) {
-            System.out.print(first + " ");
-        }
-        if (n >= 2) {
-            System.out.print(second + " ");
-        }
-        for (int i = 3; i <= n; i++) {
-            long nextTerm = first + second;  
-            System.out.print(nextTerm + " ");
-            first = second;
-            second = nextTerm; 
-        }
+        System.out.print("Enter a number: ");
+        int input = scanner.nextInt();
+        System.out.println(Fizz.getFizzBizz(input));
+        
         scanner.close();
     }
 }
