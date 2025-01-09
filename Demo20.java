fa61a0ac05046eb33c1763cd580eaa4bdd56bc67
shruthii-erram <shruthi27092005@gmail.com>
2025-01-09 13:14:34 +0530

    Add files via upload
    Multiples of 3,5 (Fizz Bizz)

---
+++ Demo20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Fizz {
+    public static String getFizzBizz(int value) {
+        if (value <= 0) {
+            return "Error";
+        } else if (value % 3 == 0 && value % 5 == 0) {
+            return "FIZZBIZZ";
+        } else if (value % 3 == 0) {
+            return "FIZZ";
+        } else if (value % 5 == 0) {
+            return "BIZZ";
+        } else {
+            return String.valueOf(value);
+        }
+    }
+}
+public class Demo20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int input = scanner.nextInt();
+        System.out.println(Fizz.getFizzBizz(input));
+        
+        scanner.close();
+    }
+}
