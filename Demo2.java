68f8fc7a60f3fef283df117dfab100da74e0d550
shruthii-erram <shruthi27092005@gmail.com>
2025-01-09 18:53:56 +0530

    Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,7 +1,10 @@
 class Demo2{
     public static void main(String[] args){
-        int a= 10;
-        int b=20;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value of a: ");
+        int a=scanner.nextInt();
+        System.out.println("Enter value of b: ");
+        int b=scanner.nextInt();
         if(a>b){
             System.out.println(a);
 
