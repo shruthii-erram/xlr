97f39ecfa47db56622cea629ae28358a6543cb3d
shruthii-erram <shruthi27092005@gmail.com>
2025-01-20 12:16:38 +0530

    storing names in arraylist

---
+++ Demo029.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+class Demo029{
+    public static void main(String[] args){
+        ArrayList<String> arr=new ArrayList<>();
+        arr.add("Shruthi");
+        arr.add("Sindhu");
+        arr.add("Siri");
+        arr.add("Sindhuja");
+        arr.add("geethu");
+        arr.add("jashhh");
+        arr.add("shailuu");
+        arr.add("aishu");
+        arr.add("renu");
+        arr.add("advaitha"); 
+        System.out.println(arr);
+    }
+}
