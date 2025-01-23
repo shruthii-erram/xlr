7044746f84811c4af42a9c4aa86a9a553f8d4256
shruthii-erram <shruthi27092005@gmail.com>
2025-01-23 12:21:08 +0530

    Multiple of 5

--- Demo019a.java
+++ Demo019a.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo19 {
+public class Demo019a {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
