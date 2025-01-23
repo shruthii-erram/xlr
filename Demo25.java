cdb2a6a21c6edfa21ec9508ea958d01d5a3231bd
shruthii-erram <shruthi27092005@gmail.com>
2025-01-23 12:32:15 +0530

    reverse a string

---
+++ Demo25.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo25{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+        char[] charArray = input.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        
+        while (start < end) {
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            start++;
+            end--;
+        }
+        String reversedString = new String(charArray);
+        System.out.println("Reversed string: " + reversedString);
+        scanner.close();
+    }
+}
