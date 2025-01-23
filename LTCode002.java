e438b6e36fb4c3a019c6855cfac7f417c4f0f708
shruthii-erram <shruthi27092005@gmail.com>
2025-01-23 12:11:53 +0530

    reversing integer from leetcode

---
+++ LTCode002.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10; 
+            x /= 10;
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0; 
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0; 
+            }
+            reversed = reversed * 10 + digit; 
+        }
+        return reversed;
+    }
+}
+public class LTCode002{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(123)); 
+        System.out.println(solution.reverse(-123)); 
+        System.out.println(solution.reverse(120)); 
+        System.out.println(solution.reverse(1534236469)); 
+    }
+}
