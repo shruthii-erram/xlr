7cbbff850745b2a59fdffb546528e262733f2d8e
shruthii-erram <shruthi27092005@gmail.com>
2025-01-23 10:45:12 +0530

    Update and rename Demo0012.java to Demo012.java

--- Demo012.java
+++ Demo012.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 	}
 
 }
- public class Demo0012 {
+ public class Demo012 {
     public static void main(String[] args) {
         CircleAreaDemo1 obj = new CircleAreaDemo1();
         obj.Area();}
