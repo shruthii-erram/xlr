04aa36d6a5789c08df0e13e0ef4429ab98dfeeb1
shruthii-erram <shruthi27092005@gmail.com>
2025-01-23 10:36:30 +0530

    Sum of the entered digits

--- Demo0017.java
+++ Demo0017.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Demo17 {
+public class Demo0017 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
