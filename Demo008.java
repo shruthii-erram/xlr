86849e3dd9e552100c9a4201a1aa3c3af5acf283
shruthii-erram <shruthi27092005@gmail.com>
2025-01-23 10:15:38 +0530

    do while loop(username and password)

--- Demo008.java
+++ Demo008.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Demo8{
+class Demo008{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String username,pwd;
