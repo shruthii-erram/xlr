778fc4842c2870492a90c7c5abf7ff7d9c781d29
shruthii-erram <shruthi27092005@gmail.com>
2025-01-25 11:58:27 +0530

    reverse a string

--- Demo025.java
+++ Demo025.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo25{
+public class Demo025{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string to reverse: ");
