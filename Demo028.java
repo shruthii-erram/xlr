612c3de49f6be9eee2fee647db87ef108f35fca5
shruthii-erram <shruthi27092005@gmail.com>
2025-01-27 11:13:48 +0530

    Fibonacci  series using recursion

---
+++ Demo028.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Demo028{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms for the Fibonacci series: ");
+        int n = scanner.nextInt();
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        scanner.close();
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n; 
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2); 
+    }
+}
+
